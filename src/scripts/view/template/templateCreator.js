2bb76c6e135de6835cde9ade786ad3aba2bca964
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2023-11-23 12:36:57 +0700

    add Login & Register page template

--- src/scripts/view/template/templateCreator.js
+++ src/scripts/view/template/templateCreator.js
@@ -17,7 +17,65 @@ const vehicleDetail = (vehicle) => {
     `;
 };
 
+const createLoginPages = () => {
+  return `
+  <div class="form-login">
+    <h1><span>Masuk</span></h1>
+    <form>
+      <div class="form-content">
+        <input name="inputEmail" type="email" class="form-control" id="inputEmail" placeholder="Masukkan Email">
+      </div>
+      <div class="form-content">
+        <input name="inputPassword" type="password" class="form-control" id="inputPassword" placeholder="Masukkan Kata Sandi">
+      </div>
+      <button id="submit-login" type="submit" class="btn">Login</button>
+    </form>
+  </div>
+  `;
+};
+
+const createRegisterPages = () => {
+  return `
+  <div class="form-register">
+    <h1><span>Daftar</span></h1>
+    <form id="registrationForm" onsubmit="return validateForm()">
+      <div class="form-content">
+        <input name="inputName" type="text" class="form-control" id="inputName" placeholder="Masukkan Nama Lengkap">
+      </div>
+      <div class="form-content">
+        <input name="inputEmail" type="email" class="form-control" id="inputEmail" placeholder="Masukkan Email">
+      </div>
+      <div class="form-content">
+        <input name="inputPassword" type="password" class="form-control" id="inputPassword" placeholder="Masukkan Kata Sandi">
+      </div>
+      <div class="form-content">
+        <input name="confirmPassword" type="password" class="form-control" id="confirmPassword" placeholder="Konfirmasi Kata Sandi">
+      </div>
+      <span id="passwordError" class="error"></span>
+      <button id="submit-register" type="submit" class="btn">Register</button>
+    </form>
+  </div>
+  <script>
+    function validateForm() {
+      var password = document.getElementById("inputPassword").value;
+      var confirmPassword = document.getElementById("confirmPassword").value;
+      var passwordError = document.getElementById("passwordError");
+
+      if (password !== confirmPassword) {
+        passwordError.textContent = "Kata Sandi tidak cocok";
+        return false;
+      } else {
+        passwordError.textContent = "";
+        return true;
+      }
+    }
+  </script>
+  `;
+};
+
 module.exports = {
   vehicleItem,
   vehicleDetail,
+  createLoginPages,
+  createRegisterPages,
 };
