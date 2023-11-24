5353fa8aa108caefd109b9ef8b9059c879960cd6
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2023-11-25 01:16:27 +0700

    add forgot password page

---
+++ src/scripts/view/pages/forgot-password.js
@@ -0,0 +1,13 @@
+const ForgotPassword = {
+  async render() {
+    return `
+        <h1>Awokaowkaowkaowk</h1>
+      `;
+  },
+
+  async afterRender() {
+
+  },
+};
+
+export default ForgotPassword;
