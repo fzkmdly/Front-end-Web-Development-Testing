957b37854b8fdbe77aab76d3af4be3e26e30fe36
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2023-12-02 20:31:07 +0700

    feat: implement register

--- src/scripts/view/pages/register.js
+++ src/scripts/view/pages/register.js
@@ -12,7 +12,7 @@ const Register = {
               <br>
               <input type="password" name="confirm_password" id="confirm_password" placeholder="Masukkan ulang kata sandi" required>
               <br>
-              <button type="submit">Daftar</button>
+              <button type="submit" id="register-btn">Daftar</button>
               <br>
               <p id="login" class="login-link">Sudah punya akun? <a href="#/login">Login</a></p>
           </form>
@@ -21,7 +21,54 @@ const Register = {
   },
 
   async afterRender() {
-    // Kode logika atau manipulasi DOM setelah rendering
+    // Add event listener for form submission
+    const registerForm = document.getElementById('register-form');
+    registerForm.addEventListener('submit', this.handleRegistration);
+  },
+
+  async handleRegistration(event) {
+    event.preventDefault();
+
+    // Get form data
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm_password').value;
+
+    // Validate password matching
+    if (password !== confirmPassword) {
+      alert('Kata sandi dan konfirmasi kata sandi tidak cocok');
+      return;
+    }
+
+    // Make API call to register
+    try {
+      const response = await fetch('https://rental-online-dicoding-cycle-5.et.r.appspot.com/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: name,
+          email,
+          password,
+        }),
+      });
+
+      const data = await response.json();
+
+      // Handle response, you might want to redirect to a different page on success
+      if (response.ok) {
+        console.log('Registration successful:', data);
+        // Redirect or perform other actions on successful registration
+      } else {
+        console.error('Registration failed:', data);
+        // Handle the error, show a message, etc.
+      }
+    } catch (error) {
+      console.error('Error during registration:', error);
+      // Handle network errors or other exceptions
+    }
   },
 };
 
