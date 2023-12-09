e1344f0d9c7c864ad2bfdc72cde120bf3ecf66e7
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2023-12-09 13:57:58 +0700

    fix: position

--- src/scripts/view/template/templateCreator.js
+++ src/scripts/view/template/templateCreator.js
@@ -1,3 +1,5 @@
+const {default: API_ENDPOINT} = require('../../globals/api-endpoint');
+
 const vehicleItem = (vehicles) => {
   const availabilityText = vehicles.isAvailable ? 'Tersedia' : 'Tidak Tersedia';
   const costAsNumber = parseFloat(vehicles.vehicleInformation.cost);
@@ -142,47 +144,58 @@ const createRegisterPages = () => {
 
 const createPartnerRegisterPages = () => {
   return `
-        <form action="#" method="post" onsubmit="return validatePartnerRegistrationForm()">
-          <h1>Daftar Partner</h1>
-          <h4>Data Diri</h4>
-          <div class="form-body">
-            <div class="form-column">
-                <label for="nama">Nama:</label>
-                <input type="text" id="nama" name="nama" required>
+      <form id="partnerForm" action="#" method="post">
+        <h1>Daftar Partner</h1>
+        <div class="form-body">
+          <div class="form-column">
+            <label for="nama">Nama sesuai KTP:</label>
+            <input type="text" id="nama" name="nama" required>
 
-                <div class="tempatTanggalLahir">
-                    <label for="ttl">Tempat</label>
-                    <label for="tanggalLahir">Tanggal Lahir:</label>
-                    <input type="text" id="tempatLahir" name="tempatLahir" required>
-                    <input type="date" id="tanggalLahir" name="tanggalLahir" required>
-                </div>
-
-                <label for="alamat">Alamat:</label>
-                <input type="text" id="alamat" name="alamat" required>
+            <label for="nik">NIK:</label>
+            <input type="text" id="nik" name="nik" required>
+            <span id="nikError" class="error"></span>
+  
+            <div class="tempatTanggalLahir">
+              <label for="tempatLahir">Tempat</label>
+              <label for="tanggalLahir">Tanggal Lahir:</label>
+              <input type="text" id="tempatLahir" name="tempatLahir" required>
+              <input type="date" id="tanggalLahir" name="tanggalLahir" min="1960-01-01" max="2010-01-01" required>
             </div>
 
-            <div class="form-column">
-                <label for="nik">NIK:</label>
-                <input type="text" id="nik" name="nik" required>
-                <span id="nikError" class="error"></span>
+            <label for="nomorSIM">Nomor SIM:</label>
+            <input type="text" id="nomorSIM" name="nomorSIM" required>
 
+            <label for="masaBerlaku">Masa Berlaku:</label>
+            <input type="date" id="masaBerlaku" name="masaBerlaku" min="2023-01-01" required>
 
-                <label for="telepon">Nomor HP:</label>
-                <input type="tel" id="telepon" name="telepon" required>
-                <span id="teleponError" class="error"></span>
+            <label for="Jenis SIM">Jenis SIM:</label>
+            <input type="text" id="jenisSIM" name="jenisSIM" placeholder="Contoh: A" required>
+        </div>
 
-                <label for="kodepos">Kode Pos:</label>
-                <input type="text" id="kodepos" name="kodepos" required>
+        <div class="form-column">
+            <label for="telepon">Nomor HP:</label>
+            <input type="tel" id="telepon" name="telepon" required>
+            <span id="teleponError" class="error"></span>
 
-                <label for="email">Email:</label>
-                <input type="email" id="emailPartner" name="email" required>
-            </div>
+            <label for="alamat">Alamat:</label>
+            <input type="text" id="alamat" name="alamat" required>
+
+            <label for="kota">Kota:</label>
+            <input type="text" id="kota" name="kota" required>
+
+            <label for="provinsi">Provinsi:</label>
+            <input type="text" id="provinsi" name="provinsi" required>
+
+            <label for="kodepos">Kode Pos:</label>
+            <input type="text" id="kodepos" name="kodepos" required>
           </div>
-
-            <button class="partnerButton" type="submit">Daftar Sekarang</button>
-        </form>
-  `;
+        </div>
+  
+        <button class="partnerButton" type="submit">Daftar Sekarang</button>
+      </form>
+    `;
 };
+
 
 const vehicleCheckin = (vehicle) =>{
   return `
@@ -242,82 +255,6 @@ const vehicleCheckin = (vehicle) =>{
         <button>Lanjutkan Pembayaran</button>
     </form>
   </section>
-  `;
-};
-
-const addRentalVehicle = () => {
-  return `
-        <form action="" method="post">
-            <h1>Data Mobil</h1>
-            <div class="inputFotoMobil">
-                <div id="imagePreviewContainer" class="imagePreviewContainer">
-                    <img src="https://i.pinimg.com/564x/00/00/5f/00005f2792a21e1b29d08638dc3d1027.jpg"  id="imagePreview" alt="Pratinjau Gambar" width="150px">
-                </div>
-                <div class="inputFotoMobilContainer">
-                    <!-- <img src="./src/public/images/icons/partner-page-icon/Vector.png" width="100px" alt="" srcset=""> -->
-                    <input type="file" id="fileInput" accept="image/*" onchange="previewImage()" >
-                </div>
-            </div>
-            <div class="bodyInputMobil">
-                <div class="bodyInputMobilLeft">
-                    <div class="add-pages-form-group">
-                        <label for="brand">Merek Kendaraan</label>
-                        <input type="text" id="brand" name="brand" placeholder="Contoh: Honda, Toyota" required>
-                    </div>
-                    <div class="add-pages-form-group">
-                        <label for="platMobil">Nomor Pelat Kendaraan</label>
-                        <input type="text" id="platMobil" name="platMobil" placeholder="Contoh: B1234PP" required>
-                    </div>
-                    <div class="add-pages-form-group">
-                        <label for="stnk">STNK</label>
-                        <input type="text" id="stnk" name="stnk" required>
-                    </div>
-                    <div class="add-pages-form-group">
-                        <label for="tipeMobil">Tipe Kendaraan</label>
-                        <input type="text" id="tipeMobil" name="tipeMobil" required>
-                    </div>
-                </div>
-                <div class="bodyInputMobilRight">
-                    <div class="add-pages-form-group">
-                        <label for="namaMobil">Seri</label>
-                        <input type="text" id="namaMobil" name="namaMobil" placeholder="Contoh: Agya, Ayla" required>
-                    </div>
-                    <div class="add-pages-form-group">
-                        <label for="tahunKeluaran">Tahun</label>
-                        <input type="text" id="tahunKeluaran" name="tahunKeluaran" placeholder="Contoh: 2023" required>
-                    </div>
-                    <div class="add-pages-form-group">
-                        <label for="bpkb">BPKB</label>
-                        <input type="text" id="bpkb" name="bpkb" required>
-                    </div>
-                    <div class="add-pages-form-group">
-                        <label for="maxPenumpang">Maksimum Penumpang</label>
-                        <input type="text" id="maxPenumpang" name="maxPenumpang" placeholder="Contoh: 4" required>
-                    </div>
-                </div>
-            </div>
-            <div class="addVehicleDesc">
-                <label for="deskripsi">Deskripsi Mobil</label>
-                <textarea id="deskripsi" name="deskripsi" rows="4" placeholder="Deskripsikan mobil Anda secara singkat" required></textarea>
-            </div>
-            <div class="footerAddForm" id="footerAddForm">
-                <div class="hargaSewaForm">
-                    <div class="add-pages-form-group">
-                        <label for="hargaSewa">Harga Sewa</label>
-                        <input type="text" id="hargaSewa" name="hargaSewa" placeholder="Contoh: 150000" required>
-                    </div>
-                </div>
-                <div class="addAlamatForm">
-                    <section class="addAlamatFormLeft">
-                        <div class="add-pages-form-group">
-                            <label for="alamat">Alamat</label>
-                            <textarea name="alamat" id="alamat" rows="4" placeholder="Masukkan alamat Anda secara lengkap"></textarea>
-                        </div>
-                    </section>
-                </div>
-            </div>
-            <button class="buttonTambahMobil">Tambahkan Mobil</button>
-        </form>
   `;
 };
 
@@ -504,7 +441,6 @@ module.exports = {
   createPartnerRegisterPages,
   createCarCollection,
   vehicleCheckin,
-  addRentalVehicle,
   partnerAfterRegistation,
   cardForListRentaled,
   createSearchBar,
